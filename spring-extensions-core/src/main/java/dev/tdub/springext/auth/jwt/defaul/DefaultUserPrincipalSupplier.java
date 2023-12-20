6ed647bd94e204b75a3a34878765b0d14e554346
Tyler Weiss <tnweiss5@gmail.com>
2023-12-19 22:17:20 -0500

    update

--- spring-extensions-core/src/main/java/dev/tdub/springext/auth/jwt/defaul/DefaultUserPrincipalSupplier.java
+++
@@ -1,8 +0,0 @@
-package dev.tdub.springext.auth.jwt.defaul;
-
-import dev.tdub.springext.auth.UserPrincipal;
-
-import java.util.function.Supplier;
-
-public interface DefaultUserPrincipalSupplier extends Supplier<UserPrincipal> {
-}
